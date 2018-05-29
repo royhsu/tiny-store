7f7cf0ad75ed1a89679e109acff7adca61c1290f
Roy Hsu <roy.hsu@tinyworld.cc>
2018-05-29 16:32:06 +0800

    add Xcode project.

---
+++ TinyStore/TinyStore/AppDelegate.swift
@@ -0,0 +1,40 @@
+//
+//  AppDelegate.swift
+//  TinyStore
+//
+//  Created by Roy Hsu on 2018/5/29.
+//  Copyright © 2018 TinyWorld. All rights reserved.
+//
+
+// MARK: - AppDelegate
+
+import UIKit
+
+@UIApplicationMain
+public final class AppDelegate: UIResponder {
+    
+    public final let window = UIWindow(frame: UIScreen.main.bounds)
+    
+}
+
+// MARK: - UIApplicationDelegate
+
+extension AppDelegate: UIApplicationDelegate {
+    
+    public final func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
+    )
+    -> Bool {
+        
+        window.rootViewController = UINavigationController(
+            rootViewController: ViewController()
+        )
+        
+        window.makeKeyAndVisible()
+        
+        return true
+            
+    }
+    
+}
