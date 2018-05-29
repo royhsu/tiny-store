9776e6962313dd80e53b569fef8a39fa64a06e2c
Roy Hsu <roy.hsu@tinyworld.cc>
2018-05-29 16:54:21 +0800

    set up the cross-platform project.

---
+++ TinyStore/Sources/TinyStore.h
@@ -0,0 +1,12 @@
+//
+//  TinyStore.h
+//  TinyStore
+//
+//  Created by Roy Hsu on 2018/5/29.
+//
+
+@import UIKit;
+
+FOUNDATION_EXPORT double TinyStoreVersionNumber;
+
+FOUNDATION_EXPORT const unsigned char TinyStoreVersionString[];
---
+++ TinyStore/Tests/iOS/TemplateTests.swift
@@ -0,0 +1,22 @@
+//
+//  TemplateTests.swift
+//  TinyStore iOS Tests
+//
+//  Created by Roy Hsu on 2018/5/29.
+//
+
+// MARK: - TemplateTests
+
+import XCTest
+
+@testable import TinyStore
+
+internal final class TemplateTests: XCTestCase {
+
+    internal func testExample() {
+
+        XCTAssert(true)
+
+    }
+
+}
